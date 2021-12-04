c9b9411638301a8e8281807a76929e575e366e45
Matteo <283122@studenti.unimore.it>
2021-12-04 23:54:03 +0100

    Ora funziona veramente

--- StringCell.java
+++ StringCell.java
@@ -3,7 +3,7 @@ public class StringCell extends Cell
     // bisognerebbe aggiungere parametri / metodi
     public StringCell(String param)
     {
-        super();
+        super(param);
         System.out.println("Creazione cella string");
     }
 }
